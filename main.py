279c6692684c6c992ae2ff09f016fc1bca888d4c
Lucas Santana Lemos <lslemos59@gmail.com>
2023-06-03 20:35:07 -0300

    i deleted everything

--- main.py
+++ main.py
@@ -1,68 +1,70 @@
 import customtkinter
 
-
-class App:
-
-    def __init__(self, window: customtkinter.CTk):
-        self.window = window
-        self.window.geometry("300x600")
-        self.window.resizable(False, False)
-        self.window.wm_title("Data")
-
-        self.user_email = {
-            "email"
-        }
-
-        App.text_Page(self=self)
-        App.login_Entry(self=self)
-        App.password_Entry(self=self)
-        App.login_Button(self=self)
-        App.register_Button(self=self)
-
-
-    def login_Button(self):
-
-        self.login_btn = customtkinter.CTkButton(self.window, text="Login", command=App.button_Click, corner_radius=20)
-        self.login_btn.pack(padx=10, pady=10)
-        
-
-    def text_Page(self):
-
-        self.text_label = customtkinter.CTkLabel(self.window, text="Fazer login", width=200, height=25)
-        self.text_label.pack(padx=10, pady=100)
-
+def app_Screen():
+    global window, dados, lista_dados
     
-    def register_Button(self):
-        
-        self.register_btn = customtkinter.CTkButton(self.window, text="Register", command=App.button_Click, corner_radius=20)
-        self.register_btn.pack(padx=10, pady=10)
+    window = customtkinter.CTk()
+    window.geometry("300x600")
+    window.resizable(False, False)
+    window.wm_title("Banco de Dados")
 
+    dados = {}
+    lista_dados = []
+
+
+def submit():
+
+    dados["email"] = email_entry.get()
+    dados["senha"] = password_entry.get()
+
+    lista_dados.append(dados.copy())
+    print(lista_dados)
+
+
+def text_Section():
+
+    text_label = customtkinter.CTkLabel(window, text="Fazer login", font=("Roboto", 20))
+    text_label.pack(padx=10, pady=180)
+
+
+def login_Button():
+
+    login_btn = customtkinter.CTkButton(window, text="Login",width=200, corner_radius=20)
+    login_btn.place(relx=0.5, rely=0.7, anchor="center")
+
+
+def register_Button():
     
-    def login_Entry(self):
-
-        self.login_entry = customtkinter.CTkEntry(self.window, placeholder_text="Seu email", width=200, corner_radius=20)
-        self.login_entry.pack(padx=10, pady=10)
-        
-
-    
-    def password_Entry(self):
-
-        self.password_entry = customtkinter.CTkEntry(self.window, placeholder_text="Sua senha", width=200, corner_radius=20, show="*")
-        self.password_entry.pack(padx=10, pady=10)
-    
+    register_btn = customtkinter.CTkButton(window, text="Register", command=submit, width=200, corner_radius=20)
+    register_btn.place(relx=0.5, rely=0.8, anchor="center")
 
 
-    def button_Click():
+def email_Input():
+    global email_entry
 
-        return print("Working")
-
-
-if __name__ == "__main__":
-    app = customtkinter.CTk()
-    gui = App(window=app)
-    app.mainloop()
+    email_entry = customtkinter.CTkEntry(window, placeholder_text="Seu email", width=200, corner_radius=20)
+    email_entry.place(relx=0.5, rely=0.5, anchor="center")
     
     
+def password_Input():
+    global password_entry
+
+    password_entry = customtkinter.CTkEntry(window, placeholder_text="Sua senha", width=200, corner_radius=20, show="*")
+    password_entry.place(relx=0.5, rely=0.6, anchor="center")
+
+
+
+app_Screen()
+text_Section()
+login_Button()
+register_Button()
+email_Input()
+password_Input()
+
+
+
+window.mainloop()
+
 
 
 
